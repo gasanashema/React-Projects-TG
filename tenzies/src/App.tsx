14f7e49555e4386d00d83210225ed249ef2325cc
Shema <shemaphilbert8@gmail.com>
2025-08-05 08:55:59 +0200

    todo app

---
+++ tenzies/src/App.tsx
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    < >
+    <div className="h-screen bg-blue-950 flex justify-center items-center">
+
+      <div className="h-[400px] w-[400px] rounded-md bg-white">
+
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default App
+        
