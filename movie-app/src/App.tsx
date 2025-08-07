ef003381c5878352c0c91cefb9ace0829ec671ef
Shema <shemaphilbert8@gmail.com>
2025-08-07 11:27:18 +0200

    Movie project finalized - added the logic to display movies

--- movie-app/src/App.tsx
+++ movie-app/src/App.tsx
@@ -1,6 +1,16 @@
+import { useState } from "react";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import MovieTopic from "./components/MovieTopic";
 function App() {
+  const API_KEY = "97ef910c8dbfc58de8ccd2f3fc7adc14";
+  const [sections, setSections] = useState({
+    "🔥 Trending Now": `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`,
+    "🎬 Now Playing": `https://api.themoviedb.org/3/movie/now_playing?api_key=${API_KEY}`,
+    "⭐ Top Rated": `https://api.themoviedb.org/3/movie/top_rated?api_key=${API_KEY}`,
+    "🔥 Popular": `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`,
+    "🚀 Upcoming": `https://api.themoviedb.org/3/movie/upcoming?api_key=${API_KEY}`,
+  })
   return (
     <>
       <div className="bg-black text-white">
@@ -8,7 +18,9 @@ function App() {
           <div className="p-5">
             <Header />
             <Banner />
-
+            {
+              Object.entries(sections).map(([title, url], ind) => <MovieTopic url={url} title={title} key={ind} />)
+            }
 
           </div>
         </div>
--- movie-app/src/components/MovieTopic.tsx
+++ movie-app/src/components/MovieTopic.tsx
@@ -1,19 +1,49 @@
-function MovieTopic(props:{title:string,url:string}) {
-    const title = props.title;
-    const url = props.url;
-    return (
-        <div id="sections">
-            <section className="mb-10">
-                <div className="flex items-center justify-between border-b border-gray-700 pb-2 mb-5">
-                    <h2 className="text-xl font-semibold">{title}</h2>
-                    <a href="#" className="text-sm font-bold text-white">See More</a>
-                </div>
-                <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5">
-                    
-                </div>
-            </section>
+import { useEffect, useState } from "react";
+import MovieCard from "./MovieCard";
+
+function MovieTopic(props: { title: string; url: string }) {
+  const API_IMAGE = "https://image.tmdb.org/t/p/w500";
+  const { title, url } = props;
+
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const res = await fetch(url);
+        const data = await res.json();
+        setMovies(data.results.slice(0, 12));
+      } catch (err) {
+        console.error("Failed to fetch movies:", err);
+      }
+    };
+
+    fetchMovies();
+  }, [url]);
+
+  return (
+    <div id="sections">
+      <section className="mb-10">
+        <div className="flex items-center justify-between border-b border-gray-700 pb-2 mb-5">
+          <h2 className="text-xl font-semibold">{title}</h2>
+          <a href="#" className="text-sm font-bold text-white">
+            See More
+          </a>
         </div>
-    )
+        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-5">
+          {movies.map((movie, index) => (
+            <MovieCard
+              key={index}
+              image={`${API_IMAGE}${movie.poster_path}`}
+              title={movie.title}
+              release_date={movie.release_date}
+              vote_average={movie.vote_average}
+            />
+          ))}
+        </div>
+      </section>
+    </div>
+  );
 }
 
-export default MovieTopic
+export default MovieTopic;
