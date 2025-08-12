d587dcae5731058c4bfda01b5c9cfe1949d8ce3e
Shema <shemaphilbert8@gmail.com>
2025-08-12 12:04:53 +0200

    dark-light mode

---
+++ mental-model/src/App.jsx
@@ -0,0 +1,31 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import {Sun} from 'react-icons/fa'
+
+function App() {
+  const [theme,setTheme] = useState(localStorage.getItem('theme'));
+
+  useEffect( () => {
+   
+     
+      localStorage.setItem('theme',theme);
+   
+  }
+    ,[theme]
+  )
+  
+  return (
+    
+      <div className={`flex justify-center items-center h-screen ${theme==='light' ? 'bg-white' : 'bg-black'}`}>
+        <div className={`p-[80px] ${theme==='light' ? 'bg-gray-100' : 'bg-gray-800'}`}>
+          <p className={`${theme==='light' ? 'text-black' : 'text-white'}`}>Vibe Coding</p> 
+          <button onClick={()=> setTheme(prev=>prev=='light'?'dark':'light')} className=' p-2 bg-amber-500 rounded'>{theme==='light' ? 'Go Dark' : 'Go Light'}</button>
+        </div>
+      </div>
+    
+  )
+}
+
+export default App
